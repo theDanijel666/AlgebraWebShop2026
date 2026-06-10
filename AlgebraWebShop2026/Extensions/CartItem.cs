084e02276c07822f2bc1019b76a5a42148c42c73
Danijel Pobi <121439317+theDanijel666@users.noreply.github.com>
2026-06-10 18:48:19 +0200

    Started cart

---
+++ AlgebraWebShop2026/Extensions/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using AlgebraWebShop2026.Models;
+
+namespace AlgebraWebShop2026.Extensions
+{
+    public class CartItem
+    {
+        public Product Product { get; set; }
+        public decimal Quantity { get; set; }
+
+        public decimal getTotal()
+        {
+            return Product.Price * (100m - Product.Discount) / 100m * Quantity;
+        }
+    }
+}
--- AlgebraWebShop2026/Program.cs
+++ AlgebraWebShop2026/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -35,6 +37,8 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapStaticAssets();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "Admin",
